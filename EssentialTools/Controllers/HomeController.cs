0e0849709f45a7df7ec597384e6678f7a13c5031
Maciej Krebs <maciej.krebs@gmail.com>
2018-04-13 22:49:36 +0200

    1

---
+++ EssentialTools/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EssentialTools.Models;
+using Ninject;
+
+namespace EssentialTools.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IValueCalculator calc;
+        private Product[] products = {
+            new Product {Name = "Kajak", Category="Sporty wodne", Price = 275M},
+            new Product {Name = "Kamizelka ratunkowa", Category="Sporty wodne", Price = 48.95M},
+            new Product {Name = "Piłka nożna", Category="Piłka nożna", Price = 19.50M},
+            new Product {Name = "Flaga narożna", Category="Piłka nożna", Price = 34.95M}
+        };
+
+        public HomeController(IValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
+        // GET: Home
+        public ActionResult Index()
+        {
+            ShoppingCart cart = new ShoppingCart(calc) { Products = products };
+            decimal totalValue = cart.CalculateProductTotal();
+            return View(totalValue);
+        }
+    }
+}
