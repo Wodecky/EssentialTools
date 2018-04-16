c9f5dd00226fe38f1b2d7ad0a8f25c31c2c516a5
Maciej Krebs <maciej.krebs@gmail.com>
2018-04-16 21:46:09 +0200

    Tests

---
+++ EssentialTools.Tests/UnitTest2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using EssentialTools.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace EssentialTools.Tests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        private Product[] products = {
+            new Product {Name = "Kajak", Category="Sporty wodne", Price = 275M},
+            new Product {Name = "Kamizelka ratunkowa", Category="Sporty wodne", Price = 48.95M},
+            new Product {Name = "Piłka nożna", Category="Piłka nożna", Price = 19.50M},
+            new Product {Name = "Flaga narożna", Category="Piłka nożna", Price = 34.95M}};
+
+        [TestMethod]
+        public void Sum_Products_Correctly()
+        {
+            var discounter = new MinimumDiscountHelper();
+            var target = new LinqValueCalculator(discounter);
+            var goalTotal = products.Sum(p => p.Price);
+
+            var result = target.ValueProducts(products);
+
+            Assert.AreEqual(goalTotal, result);
+        }
+    }
+}
